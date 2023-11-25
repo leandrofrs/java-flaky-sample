d0bc333aef5c54712121ec5c87e9e7ac3a9e3902
Leandro <leandrofisousa@gmail.com>
2023-11-25 21:06:39 +0000

    update

--- src/test/java/JUnitTestingOfMongoDBCollection.java
+++ src/test/java/JUnitTestingOfMongoDBCollection.java
@@ -1,110 +1,54 @@
-import java.util.Date; 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// Following imports are necessary for JUNIT 
-import static org.junit.Assert.assertEquals; 
-import org.junit.Test; 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// Following imports are necessary for MongoDB 
-import java.net.UnknownHostException; 
-import com.mongodb.BasicDBObject; 
-import com.mongodb.DB; 
-import com.mongodb.DBCollection; 
-import com.mongodb.DBCursor; 
-import com.mongodb.DBObject; 
-import com.mongodb.MongoClient; 
-import com.mongodb.MongoException; 
+public class UserDaoTest {
 
-public class JUnitTestingOfMongoDBCollection { 
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> userCollection;
+    private UserDao userDao;
 
-	@Test
-	public void testInsertedAndUpdatedDocument() throws Exception { 
+    @BeforeEach
+    public void setUp() {
+        // Update the connection string with your remote MongoDB URI
+        String connectionString = "mongodb+srv://<username>:<password>@<cluster-url>/<database-name>?retryWrites=true&w=majority";
+        mongoClient = MongoClients.create(connectionString);
+        database = mongoClient.getDatabase("your_test_database_name");
+        userCollection = database.getCollection("users");
+        userDao = new UserDao(connectionString);
+    }
 
-		try { 
+    @AfterEach
+    public void tearDown() {
+        mongoClient.close();
+        userDao.close();
+    }
 
-			/**** Connect to MongoDB ****/
-			// Since 2.10.0, uses MongoClient 
-			MongoClient mongo = new MongoClient("localhost", 27017); 
+    @Test
+    public void testCreateUser() {
+        User user = new User("john_doe", "john.doe@example.com");
+        userDao.createUser(user);
 
-			/**** Get database ****/
-			// if database doesn't exists, MongoDB will create it for you 
-			DB db = mongo.getDB("geeksforgeeks"); 
+        // Assuming you have a method to query the user by username or email
+        User retrievedUser = getUserByUsername(user.getUsername());
 
-			/**** Get collection / table from 'geeksforgeeks' ****/
-			// if collection doesn't exists, MongoDB will create it for you 
-			DBCollection table = db.getCollection("authors"); 
+        // Check if the user was successfully created
+        assertEquals(user.getUsername(), retrievedUser.getUsername());
+        assertEquals(user.getEmail(), retrievedUser.getEmail());
+    }
 
-			/**** Insert ****/
-			// create a document to store key and value 
-			BasicDBObject document = new BasicDBObject(); 
-			document.put("name", "author1"); 
-			document.put("age", 30); 
-			document.put("technology", "java,mongodb"); 
-			document.put("createdDate", new Date()); 
-			table.insert(document); 
-
-			/**** Find and display ****/
-			BasicDBObject searchQuery = new BasicDBObject(); 
-			searchQuery.put("name", "author1"); 
-			
-			DBCursor cursor = table.find(searchQuery); 
-			// While displaying let us check 
-			// with assert statements as well 
-			while (cursor.hasNext()) { 
-				DBObject object = cursor.next(); 
-				// Checking whether inserted name is author1 
-				assertEquals("author1", object.get("name").toString()); 
-				// Checking whether inserted age is 30 
-				assertEquals(30, Integer.parseInt(object.get("age").toString())); 
-				// Checking whether inserted technology is java,mongodb 
-				assertEquals("java,mongodb", object.get("technology").toString()); 
-				
-				
-			} 
-			
-			/**** Update ****/
-			// search document where name="author1" 
-			// and update it with new values 
-			BasicDBObject query = new BasicDBObject(); 
-			query.put("name", "author1"); 
-
-			BasicDBObject newDocument = new BasicDBObject(); 
-			// changing the technology column value 
-			newDocument.put("technology", "java,.net,mongodb"); 
-			// additionally adding a new column. 
-			// This is the advantage of mongodb. 
-			// We no need to have concrete structure 
-			// from the beginning as like RDBMS database 
-			newDocument.put("numberofposts", "10"); 
-
-			BasicDBObject updateObj = new BasicDBObject(); 
-			updateObj.put("$set", newDocument); 
-			
-			// This way we can update the document 
-			table.update(query, updateObj); 
-
-			/**** Find and display ****/
-			BasicDBObject searchQuery2 
-				= new BasicDBObject().append("name", "author1"); 
-
-			DBCursor cursor2 = table.find(searchQuery2); 
-			// Check the same as well 
-			// Now we can check whether the technology 
-			// got changed to java,.net,mongodb 
-			// and also numberofposts to 10 
-			while (cursor2.hasNext()) { 
-				DBObject object = cursor2.next(); 
-				assertEquals("author1", object.get("name").toString()); 
-				assertEquals(30, Integer.parseInt(object.get("age").toString())); 
-				assertEquals("java,.net,mongodb", object.get("technology").toString()); 
-				assertEquals("10", object.get("numberofposts").toString()); 
-			} 
-
-			
-		} catch (UnknownHostException e) { 
-			e.printStackTrace(); 
-		} catch (MongoException e) { 
-			e.printStackTrace(); 
-		} 
-
-	} 
-} 
+    private User getUserByUsername(String username) {
+        // Implement the logic to query the user from the database by username
+        // Return a User object or null if the user is not found
+        // This method is for demonstration purposes; you may replace it with your actual database query logic
+        return new User(username, "dummy.email@example.com");
+    }
+}
