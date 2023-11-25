5fe30845e7e23bf8bdf655a87fcedd4120c5cb8d
Leandro <leandrofisousa@gmail.com>
2023-11-25 21:11:32 +0000

    update

--- src/main/java/MongoDBCollection.java
+++ src/main/java/MongoDBCollection.java
@@ -1,19 +1,3 @@
-// User.java
-public class User {
-    private String username;
-    private String email;
-
-    // Constructors, getters, and setters
-
-    public User(String username, String email) {
-        this.username = username;
-        this.email = email;
-    }
-
-    // getters and setters...
-}
-
-// UserDao.java
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
