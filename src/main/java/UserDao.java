787d25c80435f1199cec5fee93818a5798c748bd
Leandro <leandrofisousa@gmail.com>
2023-11-25 21:17:33 +0000

    import user

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -3,6 +3,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import User;
 
 public class UserDao {
 
