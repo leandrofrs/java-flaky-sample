4010e9356bf9055170623669b1aa6b52d75eebf5
Leandro <leandrofisousa@gmail.com>
2023-11-26 12:42:59 +0000

    Refactor UserDaoTest to use Mockito for mocking MongoDB dependencies

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,54 +1,55 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.mockito.Mockito.*;
 
 public class UserDaoTest {
 
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    private MongoCollection<Document> userCollection;
     private UserDao userDao;
+    private MongoCollection<Document> userCollection;
 
     @BeforeEach
     public void setUp() {
-        // Update the connection string with your remote MongoDB URI
-        String connectionString = "mongodb+srv://<username>:<password>@<cluster-url>/<database-name>?retryWrites=true&w=majority";
-        mongoClient = MongoClients.create(connectionString);
-        database = mongoClient.getDatabase("your_test_database_name");
-        userCollection = database.getCollection("users");
-        userDao = new UserDao(connectionString);
-    }
+        // Create a mock MongoCollection
+        userCollection = mock(MongoCollection.class);
 
-    @AfterEach
-    public void tearDown() {
-        mongoClient.close();
-        userDao.close();
+        // Create a mock MongoDatabase
+        MongoDatabase database = mock(MongoDatabase.class);
+        when(database.getCollection("users")).thenReturn(userCollection);
+
+        // Create a UserDao instance with the mock dependencies
+        userDao = new UserDao(database);
     }
 
     @Test
     public void testCreateUser() {
+        // Arrange
         User user = new User("john_doe", "john.doe@example.com");
+        Document expectedUserDocument = new Document("username", user.getUsername())
+                .append("email", user.getEmail());
+
+        // Act
         userDao.createUser(user);
 
-        // Assuming you have a method to query the user by username or email
-        User retrievedUser = getUserByUsername(user.getUsername());
-
-        // Check if the user was successfully created
-        assertEquals(user.getUsername(), retrievedUser.getUsername());
-        assertEquals(user.getEmail(), retrievedUser.getEmail());
+        // Assert
+        verify(userCollection, times(1)).insertOne(expectedUserDocument);
     }
 
-    private User getUserByUsername(String username) {
-        // Implement the logic to query the user from the database by username
-        // Return a User object or null if the user is not found
-        // This method is for demonstration purposes; you may replace it with your actual database query logic
-        return new User(username, "dummy.email@example.com");
+    @Test
+    public void testCreateUser() {
+        // Arrange
+        User user = new User("john_doe", "john.doe@example.com");
+        Document expectedUserDocument = new Document("username", user.getUsername())
+                .append("email", user.getEmail());
+
+        // Act
+        userDao.createUser(user);
+
+        // Assert
+        verify(userCollection, times(1)).insertOne(expectedUserDocument);
     }
+
 }
